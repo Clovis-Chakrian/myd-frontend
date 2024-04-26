c92506d8e1643b48cd25585ad1fecea0a1ae5f95
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-04-25 21:26:32 -0300

    Chore: modelo base e configurações de roteamento

---
+++ src/pages/CatalogoClinicas/CatalogoClinicas.jsx
@@ -0,0 +1,18 @@
+import { Card, Space } from "antd";
+import ButtonPrimary from "../../components/Button/Button";
+import { AppLayout } from "../../components/AppLayout/AppLayout";
+
+export const CatalogoClinicas = () => {
+  return (
+    <AppLayout>
+      <Space direction="vertical" size={16}>
+        <Card title="Default size card" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+          <ButtonPrimary name={"Saiba Mais"} />
+        </Card>
+      </Space>
+    </AppLayout>
+  );
+};
