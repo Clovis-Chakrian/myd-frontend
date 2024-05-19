a435c5de094a9b2146df3b1c08cfc27a316ef096
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-05-19 20:32:24 +0000

    Refactor: Rename images trilha and uptade styles trilha

--- src/pages/Trilhas/Trilhas.jsx
+++ src/pages/Trilhas/Trilhas.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
-import img from "../../../public/icons/imgCadastro.png";
 import logo from "../../../public/icons/logo.png";
 import styles from "./Trilhas.module.css";
 import { Card } from "antd";
-import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
-import InteracaoSocial from "./TrilhasImg/Mask group (1).png"
-import rotinaSaudavel from "./TrilhasImg/Mask group (2).png"
-import  praticasGratidao from "./TrilhasImg/Mask group (3).png"
-import conexaoNatureza from "./TrilhasImg/Mask group (4).png"
-import trabalhoComunitario from "./TrilhasImg/Mask group (5).png"
-import relaxamento from "./TrilhasImg/Mask group.png"
+import InteracaoSocial from "./TrilhasImg/trilhaInteracaoSocial.png";
+import rotinaSaudavel from "./TrilhasImg/trilhaRotinaSaudavel.png";
+import praticarGratidao from "./TrilhasImg/trilhaPraticarGratidao.png";
+import conexaoNatureza from "./TrilhasImg/trilhaConexaoNatureza.png";
+import trabalhoComunitario from "./TrilhasImg/trilhaTrabalhoComunitario.png";
+import relaxamento from "./TrilhasImg/trilhaRelaxamento.png";
 
 const CardTrilhas = ({ text, imageUrl }) => {
     return (
-      <Card hoverable className={styles.card}>
-        <div className={styles.divCard}>
-          <div>
-            <h3 className={styles.h3Card}>{text}</h3>
-          </div>
-          <div style={{ maxWidth:200, maxHeight:100}}>
-            <img className={styles.imgCard} src={imageUrl} alt="Imagem"/>
-          </div>
-        </div>
-      </Card>
+        <Card hoverable className={styles.card}>
+            <Link to="/home">
+                <div className={styles.divCard}>
+                <div>
+                    <h3 className={styles.h3Card}>{text}</h3>
+                </div>
+                <div style={{ maxWidth:200, maxHeight:100}}>
+                    <img className={styles.imgCard} src={imageUrl} alt="Imagem"/>
+                </div>
+                </div>
+            </Link>
+        </Card>
     );
   };
 
@@ -37,11 +37,11 @@ export const Trilhas = () => {
       </div>
       <div className={styles.conjuntoTrilhas}>
         <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
+        <CardTrilhas text="Interação Social" imageUrl={InteracaoSocial}></CardTrilhas>
+        <CardTrilhas text="Rotina Saúdavel" imageUrl={rotinaSaudavel}></CardTrilhas>
+        <CardTrilhas text="Praticar Gratidão" imageUrl={praticarGratidao}></CardTrilhas>
+        <CardTrilhas text="Conexão com a Natureza" imageUrl={conexaoNatureza}></CardTrilhas>
+        <CardTrilhas text="Trabalho Comunitário" imageUrl={trabalhoComunitario}></CardTrilhas>
       </div>
     </div>
   );
