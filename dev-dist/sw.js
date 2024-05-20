25a2ded83722c77cdc99d69cc3d6c03cd717c281
rodrigomirandaa <digoracm@gmail.com>
2024-05-20 19:00:46 -0300

    feat:card component

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hfsu1uvit3o"
+    "revision": "0.hs5srf2j62"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
