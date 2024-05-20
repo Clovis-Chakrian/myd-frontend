11e00602f66ef0526ddd591c72887add2d0227f2
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-05-20 21:59:39 +0000

    Refactor: TrilhasImage in public, Object trilhas

--- src/pages/Trilhas/Trilhas.jsx
+++ src/pages/Trilhas/Trilhas.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import logo from "../../../public/icons/logo.png";
 import styles from "./Trilhas.module.css";
-import InteracaoSocial from "./TrilhasImg/trilhaInteracaoSocial.png";
-import rotinaSaudavel from "./TrilhasImg/trilhaRotinaSaudavel.png";
-import praticarGratidao from "./TrilhasImg/trilhaPraticarGratidao.png";
-import conexaoNatureza from "./TrilhasImg/trilhaConexaoNatureza.png";
-import trabalhoComunitario from "./TrilhasImg/trilhaTrabalhoComunitario.png";
-import relaxamento from "./TrilhasImg/trilhaRelaxamento.png";
+import InteracaoSocial from "../../../public/TrilhasImg/trilhaInteracaoSocial.png";
+import rotinaSaudavel from "../../../public/TrilhasImg/trilhaRotinaSaudavel.png";
+import praticarGratidao from "../../../public/TrilhasImg//trilhaPraticarGratidao.png";
+import conexaoNatureza from "../../../public/TrilhasImg/trilhaConexaoNatureza.png";
+import trabalhoComunitario from "../../../public/TrilhasImg/trilhaTrabalhoComunitario.png";
+import relaxamento from "../../../public/TrilhasImg/trilhaRelaxamento.png";
+
 import { CardTrilhas } from "../../components/CardTrilhas/CardTrilhas";
 
 
+
+
 export const Trilhas = () => {
+  const trilhas = [
+    { titulo: "Relaxamento", imageUrl: {relaxamento} },
+    { titulo: "Interação Socia", imageUrl: {trabalhoComunitario}},
+    { titulo: "Rotina Saúdavel", imageUrl: {conexaoNatureza}},
+    { titulo: "Praticar Gratidão", imageUrl: {praticarGratidao} },
+    { titulo: "Conexão com a Natureza", imageUrl: {rotinaSaudavel} },
+    { titulo: "Trabalho Comunitário", imageUrl: {InteracaoSocial} }
+  ];
 
   return (
     <div className={styles.wrapper}>
@@ -21,12 +32,7 @@ export const Trilhas = () => {
         </div>
       </div>
       <div className={styles.conjuntoTrilhas}>
-        <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
-        <CardTrilhas text="Interação Social" imageUrl={InteracaoSocial}></CardTrilhas>
-        <CardTrilhas text="Rotina Saúdavel" imageUrl={rotinaSaudavel}></CardTrilhas>
-        <CardTrilhas text="Praticar Gratidão" imageUrl={praticarGratidao}></CardTrilhas>
-        <CardTrilhas text="Conexão com a Natureza" imageUrl={conexaoNatureza}></CardTrilhas>
-        <CardTrilhas text="Trabalho Comunitário" imageUrl={trabalhoComunitario}></CardTrilhas>
+      {trilhas.map((trilha, index) => ( <CardTrilhas index={index} text={trilha.titulo} imageUrl={trilha.imageUrl}></CardTrilhas>))}
       </div>
     </div>
   );
