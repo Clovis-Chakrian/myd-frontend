9fe86557b34d2f01a764f17885862cfd3955f488
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-05-20 21:14:04 +0000

    Refactor: CardTrilha Component

--- src/pages/Trilhas/Trilhas.jsx
+++ src/pages/Trilhas/Trilhas.jsx
@@ -1,31 +1,14 @@
 import React, { useState } from "react";
 import logo from "../../../public/icons/logo.png";
 import styles from "./Trilhas.module.css";
-import { Card } from "antd";
-import { Link } from "react-router-dom";
 import InteracaoSocial from "./TrilhasImg/trilhaInteracaoSocial.png";
 import rotinaSaudavel from "./TrilhasImg/trilhaRotinaSaudavel.png";
 import praticarGratidao from "./TrilhasImg/trilhaPraticarGratidao.png";
 import conexaoNatureza from "./TrilhasImg/trilhaConexaoNatureza.png";
 import trabalhoComunitario from "./TrilhasImg/trilhaTrabalhoComunitario.png";
 import relaxamento from "./TrilhasImg/trilhaRelaxamento.png";
+import { CardTrilhas } from "../../components/CardTrilhas/CardTrilhas";
 
-const CardTrilhas = ({ text, imageUrl }) => {
-    return (
-        <Card hoverable className={styles.card}>
-            <Link to="/home">
-                <div className={styles.divCard}>
-                <div>
-                    <h3 className={styles.h3Card}>{text}</h3>
-                </div>
-                <div style={{ maxWidth:200, maxHeight:100}}>
-                    <img className={styles.imgCard} src={imageUrl} alt="Imagem"/>
-                </div>
-                </div>
-            </Link>
-        </Card>
-    );
-  };
 
 export const Trilhas = () => {
 
@@ -33,7 +16,9 @@ export const Trilhas = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <img src={logo} alt="logo" className={styles.logo} />
-        <h2 className={styles.subtitulo}>Para começarmos, escolha uma trilha abaixo : </h2>
+        <div className={styles.caixaSubTit}>
+          <h2 className={styles.subtitulo}>Para começarmos, escolha uma trilha abaixo : </h2>
+        </div>
       </div>
       <div className={styles.conjuntoTrilhas}>
         <CardTrilhas text="Relaxamento" imageUrl={relaxamento}></CardTrilhas>
