97eea6a98f9c50356b573abc64b742b8a6a253fe
AgedAnna <annabeatryz12345@gmail.com>
2024-05-20 22:53:25 -0300

    chore: pipe vercel

--- src/pages/Config/Config.jsx
+++ src/pages/Config/Config.jsx
@@ -1,19 +1,6 @@
-import { Card, Space } from "antd";
-import ButtonPrimary from "../../components/Button/Button";
 import { AppLayout } from "../../components/AppLayout/AppLayout";
-import { PageHeader } from "antd";
 import React from "react";
 
 export const Config = () => {
-  return (
-    <AppLayout>
-      <PageHeader
-        className="site-page-header"
-        onBack={() => null}
-        title="Title"
-        subTitle="This is a subtitle"
-      />
-      <Space direction="vertical" size={16}></Space>
-    </AppLayout>
-  );
+  return <AppLayout children={"Aqui"}></AppLayout>;
 };
