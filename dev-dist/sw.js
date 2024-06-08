e04f28a2ab1f1dda9cfb72fc1440325843843e9e
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-06-08 11:17:33 -0300

    feature: atualizando a home e conectando com desafios

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gnn0lpgvjog"
+    "revision": "0.ip14b63sljo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Checkbox, Carousel, Alert, Spin } from "antd";
+import { Row, Col, Checkbox, Carousel } from "antd";
 import Button from "../../components/Button/Button.jsx";
 import style from "./Home.module.css";
 import { ContentLayout } from "../../components/ContentLayout/ContentLayout";
@@ -8,28 +8,12 @@ import { ButtonEmoji } from "../../components/ButtonEmoji/ButtonEmoji.jsx";
 import mariaImg from "../../../public/events/maria.jpg";
 import rockImg from "../../../public/events/rock.png";
 import taruImg from "../../../public/events/taru.jpg";
-import { httpClientJwt } from "../../services/httpClient.js";
 
 export const Home = () => {
+  
   const [desafios, setDesafios] = useState([]);
   const [erros, setErros] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const listDesafio = async () => {
-    setIsLoading(true);
-    try {
-      const response = await httpClientJwt.get("/desafio");
-      setDesafios(response.data);
-    } catch (err) {
-      setErros([...err.response.data.erros]);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    listDesafio();
-  }, []);
 
   const emojis = [
     { title: "muito feliz", img: "😊", id: 1, color: "#E01754" },
@@ -45,40 +29,50 @@ export const Home = () => {
     { alt: "taruImg", img: taruImg, id: 3 },
   ];
 
+  const listDesafio = async () => {
+    setIsLoading(true);
+    try {
+      const response = await httpClientJwt.get("/desafios");
+      setDesafios(response.data);
+    } catch (err) {
+      setErros([...err.response.data.erros]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    listDesafio();
+  }, []);
+
+console.log(desafios)
   return (
-    <Spin spinning={isLoading}>
-      <ContentLayout>
-        <div className={style.configContainer}>
-          <Row className={style.rowOne} justify="center">
-            <Col xs={24} md={16}>
-              <CardLayout
-                cardTitle={"Pronta para avaliar seu dia?"}
-                className={style.card}
-              >
-                <Row style={{ fontWeight: "bold" }}>Olá Ana! 🥰</Row>
-                <Row>Como você está se sentindo hoje?</Row>
-                <Row justify="center">
-                  {emojis.map((emoji) => (
-                    <Col key={emoji.id} xs={4}>
-                      <ButtonEmoji icon={emoji.img} background={emoji.color} />
-                    </Col>
-                  ))}
-                </Row>
-              </CardLayout>
-            </Col>
-          </Row>
+    <ContentLayout>
+      <div className={style.configContainer}>
+        <Row className={style.rowOne} justify="center" gutter={[16, 16]}>
+          <Col xs={24} md={12}>
+            <CardLayout cardTitle={"Pronta para avaliar seu dia?"} className={style.card}>
+              <Row style={{ fontWeight: "bold" }}>Olá Ana! 🥰</Row>
+              <Row>Como você está se sentindo hoje?</Row>
+              <Row justify="center">
+                {emojis.map((emoji) => (
+                  <Col key={emoji.id} xs={4}>
+                    <ButtonEmoji icon={emoji.img} background={emoji.color} />
+                  </Col>
+                ))}
+              </Row>
+            </CardLayout>
+          </Col>
+          <Col xs={24} md={12}>
+            <CardLayout cardTitle={"Cronometro"} className={style.card}>
+              <Row style={{ fontWeight: "bold" }} justify="center">23:59</Row>
+            </CardLayout>
+          </Col>
+        </Row>
 
-          <Row className={style.rowOne} justify="center">
-            <Col xs={24} md={16}>
-              <CardLayout cardTitle={"Cronômetro"} className={style.card}>
-                <Row style={{ fontWeight: "bold" }}>23:59</Row>
-              </CardLayout>
-            </Col>
-          </Row>
-
-          <Row className={style.rowOne} justify="center">
-            <Col sm={11}>
-              <CardLayout cardTitle={"Desafios do dia"} className={style.card}>
+        <Row className={style.rowOne} justify="center" gutter={[16, 16]}>
+          <Col xs={24} md={12}>
+            <CardLayout cardTitle={"Desafios do dia"} className={style.card}>
                 {erros.length > 0 && (
                   <Alert message={erros.join(", ")} type="error" />
                 )}
@@ -87,41 +81,40 @@ export const Home = () => {
                     <Checkbox /> {des.titulo}
                   </p>
                 ))}
-                <Button name={"Veja tudo"} className={style.Button} />
-              </CardLayout>
-            </Col>
-            <Col sm={11} className={style.rowOne}>
-              <CardLayout cardTitle={"Espaço zen"} className={style.card}>
-                <p>Quanto tempo você precisa relaxar?</p>
-                <h1>--h --min</h1>
-                <Button name={"Vamos começar"} className={style.Button} />
-              </CardLayout>
-            </Col>
-          </Row>
-          <Row className={style.rowOne} justify="center">
-            <Col xs={24} md={16}>
-              <CardLayout
-                cardTitle={"Eventos"}
-                stylesCard={style.cardCarousel}
-              >
-                <Carousel autoplay>
-                  {events.map((event) => (
-                    <div key={event.id}>
-                      <img
-                        src={event.img}
-                        alt={event.alt}
-                        className={style.eventImage}
-                      />
-                    </div>
-                  ))}
-                </Carousel>
-              </CardLayout>
-            </Col>
-          </Row>
-        </div>
-      </ContentLayout>
-    </Spin>
+              <Button name={"Veja tudo"} className={style.Button} />
+            </CardLayout>
+          </Col>
+          <Col xs={24} md={12}>
+            <CardLayout cardTitle={"Espaço zen"} className={style.card}>
+              <p>Quanto tempo você precisa relaxar?</p>
+              <h1>--h --min</h1>
+              <Button name={"Vamos começar"} className={style.Button} />
+            </CardLayout>
+          </Col>
+        </Row>
+
+        <Row className={style.rowOne} justify="center">
+          <Col xs={24}>
+            <CardLayout cardTitle={"Eventos"} className={style.cardCarousel}>
+              <Carousel autoplay>
+                {events.map((event) => (
+                  <div key={event.id}>
+                    <img
+                      src={event.img}
+                      alt={event.alt}
+                      className={style.eventImage}
+                    />
+                  </div>
+                ))}
+              </Carousel>
+            </CardLayout>
+          </Col>
+        </Row>
+      </div>
+    </ContentLayout>
   );
 };
+
+
 
 
