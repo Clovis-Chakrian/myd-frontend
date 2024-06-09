2b974cb5f2d44a515fe7069aad7ea59fdb2891ba
AgedAnna <annabeatryz12345@gmail.com>
2024-06-08 23:02:30 -0300

    feat: responsive chat

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,20 +1,41 @@
-import { Col, Flex, Input, Layout, Row, Avatar, Typography, Badge } from "antd";
-import { UserOutlined, SearchOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
+import {
+  Col,
+  Flex,
+  Input,
+  Layout,
+  Row,
+  Avatar,
+  Typography,
+  Badge,
+  Button,
+} from "antd";
+import {
+  UserOutlined,
+  SearchOutlined,
+  WechatOutlined,
+  UsergroupAddOutlined,
+} from "@ant-design/icons";
 import { CardContato } from "./CardContato/ContatoCard";
-import style from "./Chat.module.css";
 import { Mensagem } from "./Mensagem/Mensagem";
 import { mensagensExemplo } from "./mensagensExemplo";
 import { contatosExemplo } from "./contatosExemplo";
 import { BotaoEnviar } from "./BotaoEnviar/BotaoEnviar";
-import { BotaoAnexo } from "./BotaoAnexo/BotaoAnexo"
-import { useState } from "react";
+import { BotaoAnexo } from "./BotaoAnexo/BotaoAnexo";
+import { Link } from "react-router-dom";
+import style from "./Chat.module.css";
 
 export const Chat = () => {
-  const [mensagem, setMensagem] = useState();
+  const [mensagem, setMensagem] = useState("");
   const [mensagens, setMensagens] = useState(mensagensExemplo);
+  const [showSidebar, setShowSidebar] = useState(false);
 
-  function enviarMensagem() {
-    if (mensagem == null || mensagem == "") {
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const enviarMensagem = () => {
+    if (!mensagem.trim()) {
       return;
     }
 
@@ -23,16 +44,24 @@ export const Chat = () => {
       autorId: 0,
       nomeAutor: "Usuário Atual",
       conteudo: mensagem,
-      hora: "12h"
+      hora: "12h",
     };
 
     setMensagens([...mensagens, novaMensagem]);
     setMensagem("");
-  }
+  };
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setShowSidebar(window.innerWidth >= 992);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
 
   return (
     <Row>
-      <Col span={6}>
+      <div className={`${style.colChats} ${showSidebar ? style.show : ""}`}>
         <Flex align="center" justify="center">
           <Input
             className={style.inputBuscarMensagem}
@@ -40,74 +69,106 @@ export const Chat = () => {
             suffix={<SearchOutlined />}
           />
         </Flex>
-
         <Flex className={style.contatosContainer} vertical gap={20}>
-          {contatosExemplo.map(contato => {
-            return (
-              <CardContato
-                key={contato.contatoId}
-                contatoNome={contato.contatoNome}
-                ultimaMensagem={contato.ultimaMensagem}
-                ultimaMensagemHora={contato.ultimaMensagemHora}
-              />
-            )
-          })}
+          {contatosExemplo.map((contato) => (
+            <CardContato
+              key={contato.contatoId}
+              contatoNome={contato.contatoNome}
+              ultimaMensagem={contato.ultimaMensagem}
+              ultimaMensagemHora={contato.ultimaMensagemHora}
+            />
+          ))}
         </Flex>
-      </Col>
+      </div>
 
-      <Col span={18}>
-        <Flex vertical>
-          <Layout.Header className={style.cabecalhoChat}>
-            <div></div>
+      <div style={{ flex: 1 }}>
+        <Col span={24}>
+          <Flex vertical>
+            <Layout.Header className={style.cabecalhoChat}>
+              <Button
+                type="primary"
+                shape="circle"
+                onClick={toggleSidebar}
+                className={style.buttonOpenChatsOpen}
+              >
+                <WechatOutlined />
+              </Button>
 
-            <Flex align="center" justify="center" gap={20}>
-              <Badge count={1} color="#000">
-                <Avatar shape="circle" icon={<UserOutlined />} />
-              </Badge>
+              <Link to="/squads">
+                <Button className={style.buttonOpenChats} shape="circle">
+                  <UsergroupAddOutlined />
+                </Button>
+              </Link>
 
-              <Flex vertical align="center">
-                <Typography.Title className={style.nomeSquad} level={5}>Marmotas</Typography.Title>
-                <Typography.Text className={style.numeroSquad}>Squad 0067</Typography.Text>
+              <Flex align="center" justify="center" gap={20}>
+                <Badge count={1} color="#000">
+                  <Avatar shape="circle" icon={<UserOutlined />} />
+                </Badge>
+
+                <Flex vertical align="center">
+                  <Typography.Title className={style.nomeSquad} level={5}>
+                    Marmotas
+                  </Typography.Title>
+                  <Typography.Text className={style.numeroSquad}>
+                    Squad 0067
+                  </Typography.Text>
+                </Flex>
               </Flex>
-            </Flex>
-
-            <Flex>
               <Typography.Text>5/5 Completos</Typography.Text>
-            </Flex>
-          </Layout.Header>
-          <Flex className={style.mensagensContainer} vertical>
-            {mensagens.map(mensagem => {
-              return (
+            </Layout.Header>
+            <Flex className={style.mensagensContainer} vertical>
+              {mensagens.map((mensagem) => (
                 <Mensagem
                   key={mensagem.mensagemId}
                   nomeAutor={mensagem.nomeAutor}
                   conteudo={mensagem.conteudo}
                   hora={mensagem.hora}
                   fotoPerfil="http://localhost:5173/src/pages/SobreNos/membros/Rodrigo.jpg"
-                  mostrarFotoPerfil={mensagens.indexOf(mensagem) == 0 ? true : mensagens[mensagens.indexOf(mensagem) - 1].nomeAutor !== mensagem.nomeAutor}
-                  mostrarNomeAutor={mensagens.indexOf(mensagem) == 0 ? true : mensagens[mensagens.indexOf(mensagem) - 1].nomeAutor !== mensagem.nomeAutor}
-                  mostrarHora={mensagens[mensagens.indexOf(mensagem) + 1] != null ? mensagens[mensagens.indexOf(mensagem) + 1].nomeAutor != mensagem.nomeAutor : false}
-                  ultimaMensagemSeguida={mensagens[mensagens.indexOf(mensagem) + 1] != null ? mensagens[mensagens.indexOf(mensagem) + 1].nomeAutor != mensagem.nomeAutor : false}
-                  usuarioAtual={mensagem.autorId == 0}
+                  mostrarFotoPerfil={
+                    mensagens.indexOf(mensagem) === 0 ||
+                    mensagens[mensagens.indexOf(mensagem) - 1].nomeAutor !==
+                      mensagem.nomeAutor
+                  }
+                  mostrarNomeAutor={
+                    mensagens.indexOf(mensagem) === 0 ||
+                    mensagens[mensagens.indexOf(mensagem) - 1].nomeAutor !==
+                      mensagem.nomeAutor
+                  }
+                  mostrarHora={
+                    mensagens[mensagens.indexOf(mensagem) + 1] != null
+                      ? mensagens[mensagens.indexOf(mensagem) + 1].nomeAutor !==
+                        mensagem.nomeAutor
+                      : false
+                  }
+                  ultimaMensagemSeguida={
+                    mensagens[mensagens.indexOf(mensagem) + 1] != null
+                      ? mensagens[mensagens.indexOf(mensagem) + 1].nomeAutor !==
+                        mensagem.nomeAutor
+                      : false
+                  }
+                  usuarioAtual={mensagem.autorId === 0}
                 />
-              );
-            })}
-          </Flex>
+              ))}
+            </Flex>
 
-          <Flex align="center" justify="center">
-            <Input
-              prefix={<BotaoAnexo clickFunction={() => alert("Você inseriu um anexo!")} />}
-              placeholder="Digite sua mensagem..."
-              className={style.mensagemInput}
-              onChange={e => setMensagem(e.target.value)}
-              value={mensagem}
-              onPressEnter={enviarMensagem}
-              suffix={<BotaoEnviar clickFunction={enviarMensagem} />}
-            />
+            <Flex align="center" justify="center">
+              <Input
+                prefix={
+                  <BotaoAnexo
+                    clickFunction={() => alert("Você inseriu um anexo!")}
+                  />
+                }
+                placeholder="Digite sua mensagem..."
+                className={style.mensagemInput}
+                onChange={(e) => setMensagem(e.target.value)}
+                value={mensagem}
+                onPressEnter={enviarMensagem}
+                suffix={<BotaoEnviar clickFunction={enviarMensagem} />}
+              />
+            </Flex>
           </Flex>
-        </Flex>
-
-      </Col>
+        </Col>
+      </div>
     </Row>
   );
 };
